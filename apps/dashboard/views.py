cd964037630f16ce1df3404b1b559e7f1b9d60f7
Paul Osman <paul@eval.ca>
2010-07-15 16:43:49 -0400

    Require login to visit dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -1,5 +1,8 @@
 import jingo
 
+from django.contrib.auth.decorators import login_required
+
+@login_required
 def dashboard(request):
     """Stub View"""
     return jingo.render(request, 'dashboard.html')
