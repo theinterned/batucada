46b0db477ff1c6825b0dab98fd21b1ba680f4054
Paul Osman <paul@eval.ca>
2010-07-21 22:51:08 -0400

    Make the dashboard.views.index view the default (root) view

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls.defaults import *
 
-from dashboard import views
-
 urlpatterns = patterns('',
-    (r'^dashboard/', views.dashboard),
+    (r'^$', 'dashboard.views.index'),
 )
