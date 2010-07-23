e3762b1167421fa7bbdddbff7602a18e88b2f91f
Paul Osman <paul@eval.ca>
2010-07-22 21:53:56 -0400

    Add a personalized message just to test user models

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -3,4 +3,6 @@ import jingo
 def index(request):
     if not request.user.is_authenticated():
         return jingo.render(request, 'dashboard/signin.html')
-    return jingo.render(request, 'dashboard/dashboard.html')
+    return jingo.render(request, 'dashboard/dashboard.html', {
+        'full_name' : request.user.get_full_name()
+    })
