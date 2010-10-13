b1a43b6ceefe7a977f31a2801bb52a17adf1b467
Paul Osman <paul@eval.ca>
2010-10-13 14:24:52 -0400

    Give names to activity stream urlconf patterns so we can reference them with url tag

--- apps/activity/urls.py
+++ apps/activity/urls.py
@@ -4,6 +4,9 @@ from activity.feeds import UserActivityAtomFeed, ObjectActivityAtomFeed
 
 urlpatterns = patterns('',
     (r'activity/(?P<activity_id>\d+)/$', 'activity.views.index'),
-    (r'(?P<username>[\w ]+)/stream/$', UserActivityAtomFeed()),
-    (r'(?P<object_id>\d+)/stream/$', ObjectActivityAtomFeed()),
+                       
+    url(r'(?P<username>[\w ]+)/stream/$', UserActivityAtomFeed(),
+        name='activity_feed_user'),
+    url(r'(?P<object_id>\d+)/stream/$', ObjectActivityAtomFeed(),
+        name='activity_feed_object'),
 )
