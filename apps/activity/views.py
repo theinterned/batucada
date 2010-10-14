00d89a46782f8fcfc609abd3f138cdc19c5fc930
Paul Osman <paul@eval.ca>
2010-10-14 15:33:43 -0400

    Now there, that doesn't have to be so verbose

--- apps/activity/views.py
+++ apps/activity/views.py
@@ -1,16 +1,11 @@
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 
 from activity.models import Activity
 
 def index(request, activity_id):
     """HTML representation of a single activity."""
-    try:
-        activity = Activity.objects.get(id=activity_id)
-        
-        return render_to_response('activity/index.html', {
-            'activity': activity
-        }, context_instance=RequestContext(request))
-    except Activity.DoesNotExist:
-        return HttpResponse(status=404)
+    activity = get_object_or_404(Activity, id=activity_id)
+    return render_to_response('activity/index.html', {
+        'activity': activity
+    }, context_instance=RequestContext(request))
