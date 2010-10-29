05533311c4c512edec0e9231bf3825f8c863a409
Paul Osman <paul@eval.ca>
2010-10-29 23:15:32 +0100

    Add some fields to the project model. Also modify the form so that if a project is not 'featured', certain fields will not be editable

--- apps/projects/forms.py
+++ apps/projects/forms.py
@@ -2,9 +2,20 @@ from django import forms
 
 from projects.models import Project
 
-class ProjectForm(forms.ModelForm):
+class ProtectedProjectForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(ProtectedProjectForm, self).__init__(*args,**kwargs)
+        
+        protected = getattr(self.Meta, 'protected')
+        project = kwargs.get('instance', None)
+
+        if not project.featured:
+            for field in protected:
+                self.fields.pop(field)
+
+class ProjectForm(ProtectedProjectForm):
     class Meta:
         model = Project
-        exclude = ('created_by', 'slug')
-    
+        exclude = ('created_by', 'slug', 'featured')
+        protected = ('template', 'css')
 
--- apps/projects/models.py
+++ apps/projects/models.py
@@ -3,15 +3,23 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
 from django.template.defaultfilters import slugify
-    
+
+from relationships.models import followers
+
 class Project(models.Model):
     """Placeholder model for projects."""
     object_type = 'http://drumbeat.org/activity/schema/1.0/project'
     generalized_object_type = 'http://activitystrea.ms/schema/1.0/group'
+    
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
     description = models.TextField()
+    call_to_action = models.TextField()
     created_by = models.ForeignKey(User, related_name='projects')
+
+    featured = models.BooleanField()
+    template = models.TextField()
+    css = models.TextField()
     
     def __unicode__(self):
         return self.name
@@ -35,6 +43,13 @@ class Project(models.Model):
             count += 1
         super(Project, self).save()
 
+Project.followers = followers
+
+class Link(models.Model):
+    title = models.CharField(max_length=250)
+    url = models.URLField()
+    project = models.ForeignKey(Project)
+    
 def project_creation_handler(sender, **kwargs):
     project = kwargs.get('instance', None)
     if not isinstance(project, Project):
