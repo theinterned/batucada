801ed41587452bc3215b868850c1153a0f513cf2
Paul Osman <paul@eval.ca>
2010-11-12 08:07:40 -0500

    Register Status model with admin application

--- apps/statuses/models.py
+++ apps/statuses/models.py
@@ -1,26 +1,31 @@
+from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.timesince import timesince
 
+
 class Status(models.Model):
     author = models.ForeignKey(User)
     object_type = 'http://activitystrea.ms/schema/1.0/status'
-    
+
     status = models.CharField(max_length=1024)
     timestamp = models.DateTimeField(auto_now_add=True)
-    
+
     def __unicode__(self):
         return self.status
 
     @models.permalink
     def get_absolute_url(self):
         return ('statuses_show', (), {
-            'status_id': self.pk
+            'status_id': self.pk,
         })
 
     def timesince(self, now=None):
         return timesince(self.timestamp, now)
+
+admin.site.register(Status)
+
 
 def status_creation_handler(sender, **kwargs):
     status = kwargs.get('instance', None)
