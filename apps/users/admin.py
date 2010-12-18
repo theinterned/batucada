12aa99e6346d32bfce5b759da6c5ced5ad8881fb
Paul Osman <paul@eval.ca>
2010-12-17 23:06:40 -0500

    Create backends module in users application. Create new UserProfile model in users application.

--- apps/users/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from users.models import ConfirmationToken
-
-class ConfirmationTokenAdmin(admin.ModelAdmin):
-    pass
-
-admin.site.register(ConfirmationToken, ConfirmationTokenAdmin)
