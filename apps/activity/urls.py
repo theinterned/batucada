7966bef7314ee34ff2db98352b5380db5a8938a9
Paul Osman <paul@eval.ca>
2010-12-29 17:28:22 -0500

    Basic Atom feed for user activity

--- apps/activity/urls.py
+++ apps/activity/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls.defaults import patterns, url
 
+from activity.feeds import UserActivityFeed
+
 
 urlpatterns = patterns('',
-    url(r'^(?P<activity_id>[\d]+)/', 'activity.views.index',
+    url(r'^activity/(?P<activity_id>[\d]+)/', 'activity.views.index',
         name='activity_index'),
-    url(r'^delete/$', 'activity.views.delete',
+    url(r'^activity/delete/$', 'activity.views.delete',
         name='activity_delete'),
+    url(r'^(?P<username>[\w-]+)/feed', UserActivityFeed(),
+        name='activity_user_feed'),
 )
