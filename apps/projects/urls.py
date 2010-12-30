3b1d8e1d7f043e97195bb0a6033673b1bb8298f3
Paul Osman <paul@eval.ca>
2010-12-30 16:29:35 -0500

    Adding project detailed description editing

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -5,11 +5,16 @@ urlpatterns = patterns('',
       name='projects_gallery'),
   url(r'^create/$', 'projects.views.create',
       name='projects_create'),
-  url(r'^(?P<slug>[\w-]+)/edit/$', 'projects.views.edit',
-      name='projects_edit'),
   url(r'^(?P<slug>[\w-]+)/$', 'projects.views.show',
       name='projects_show'),
   url(r'^(?P<slug>[\w-]+)/contactfollowers/$',
       'projects.views.contact_followers',
       name='projects_contact_followers'),
+
+  # Project Edit URLs
+  url(r'^(?P<slug>[\w-]+)/edit/$', 'projects.views.edit',
+      name='projects_edit'),
+  url(r'^(?P<slug>[\w-]+)/edit_description/$',
+      'projects.views.edit_description',
+      name='projects_edit_description'),
 )
