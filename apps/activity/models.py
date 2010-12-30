e41e0e4a0b7f999e90eb7f775bf92e12a0302bb5
Paul Osman <paul@eval.ca>
2010-12-29 19:22:06 -0500

    More work on feeds. Next up is activity streams data

--- apps/activity/models.py
+++ apps/activity/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+
 from drumbeat.models import ModelBase
+from activity import schema
 
 
 class RemoteObject(models.Model):
@@ -22,3 +25,20 @@ class Activity(ModelBase):
     remote_object = models.ForeignKey(RemoteObject, null=True)
     parent = models.ForeignKey('self', null=True)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('activity_index', (), {
+            'activity_id': self.pk,
+        })
+
+    def __unicode__(self):
+        if self.target_user and self.verb == schema.verbs['follow']:
+            return "%s %s %s" % (
+                self.actor.name, schema.past_tense['follow'],
+                self.target_user.name)
+        if self.status:
+            return self.status.status
+        elif self.remote_object:
+            return self.remote_object.title
+        return _("%s activity performed by %s") % (self.verb, self.actor.name)
