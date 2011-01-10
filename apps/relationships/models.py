c372a488fcd248d12288c0d6f230c6f203664c2c
Paul Osman <paul@eval.ca>
2011-01-09 21:51:39 -0500

    Touching up emails. Subject to change, but these will do for now

--- apps/relationships/models.py
+++ apps/relationships/models.py
@@ -6,6 +6,7 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.translation import ugettext as _
+from django.template.loader import render_to_string
 
 from drumbeat.models import ModelBase
 from activity.models import Activity
@@ -57,23 +58,35 @@ def follow_handler(sender, **kwargs):
     rel = kwargs.get('instance', None)
     if not isinstance(rel, Relationship):
         return
+    user_subject = _("%(name)s is following you on Drumbeat!" % {
+        'name': rel.source.name,
+    })
+    project_subject = _("%(name)s is following your project on Drumbeat!" % {
+        'name': rel.source.name,
+    })
     activity = Activity(actor=rel.source,
                         verb='http://activitystrea.ms/schema/1.0/follow')
+    subject = _(u"%(name)s is now following")
     if rel.target_user:
         activity.target_user = rel.target_user
         user = rel.target_user
         pref_key = 'no_email_new_follower'
+        subject = user_subject
     else:
         activity.project = rel.target_project
         user = rel.target_project.created_by
         pref_key = 'no_email_new_project_follower'
+        subject = project_subject
     activity.save()
 
     preferences = AccountPreferences.objects.filter(user=user)
     for pref in preferences:
         if pref.value and pref.key == pref_key:
             return
-    subject = "New Follower"  # todo - write copy
-    body = ""
+
+    body = render_to_string("relationships/emails/new_follower.txt", {
+        'user': rel.source,
+        'project': rel.target_project,
+    })
     SendUserEmail.apply_async((user, subject, body))
 post_save.connect(follow_handler, sender=Relationship)
