5985efc961b678145f2f14bf677ad8fc273c6e9a
theinterned <ned@theinterned.net>
2011-01-14 13:13:49 -0500

    adding abortive tests to push for paul to take a look at

--- media/js/script.js
+++ media/js/script.js
@@ -179,11 +179,33 @@ $(document).ready(function() {
     $('#user-nav').find('li.menu').bind('click', function(event) {
 	$(this).toggleClass('open');
     });
+    // modals using jQueryUI dialog
+    $('.button.openmodal').live('click', function(){
+        var url = this.href;
+        //var url = "/en-US/test/"; //testing purposes
+        var selector = '.modal';
+        var url_fragment =  url + ' ' + selector;
+        var dialog = $('<div style=""></div>').appendTo('body');
+        // load remote content
+        dialog.load(
+            url_fragment, 
+            {},
+            function (responseText, textStatus, XMLHttpRequest) {
+                log(responseText);
+                dialog.dialog({
+                    draggable: true,
+                    
+                });
+            }
+        );
+        //prevent the browser to follow the link
+        return false;
+    })
 
     // find submit buttons and bind them to an event that submits their form
-    $('.submit-button').bind('click', function(e) {
-	$(e.target).parent('form[method="post"]').first().submit();
-    });
+    //     $('.submit-button').bind('click', function(e) {
+    // $(e.target).parent('form[method="post"]').first().submit();
+    //     });
 });
 
 
