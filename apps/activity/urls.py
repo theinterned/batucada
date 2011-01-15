cc2712a806e6c4452021bc24e9d2114ec3a1f62f
Paul Osman <paul@eval.ca>
2011-01-14 20:54:10 -0500

    Wired up report abuse functionality

--- apps/activity/urls.py
+++ apps/activity/urls.py
@@ -4,17 +4,10 @@ from activity import feeds
 
 
 urlpatterns = patterns('',
-    url(r'^activity/(?P<activity_id>[\d]+)/', 'activity.views.index',
-        name='activity_index'),
-    url(r'^activity/delete/$', 'activity.views.delete',
-        name='activity_delete'),
     url(r'^(?P<username>[\w-]+)/feed$', feeds.UserActivityFeed(),
         name='activity_user_feed'),
     url(r'^projects/(?P<project>[\w-]+)/feed$', feeds.ProjectActivityFeed(),
         name='activity_project_feed'),
     url(r'^feed$', feeds.DashboardFeed(),
-        name='activity_dashboard_feed'),	
-	url(r'^activity/report/$',
-		'django.views.generic.simple.direct_to_template', {'template': 'activity/report_received.html'},
-		name='activity_report')
+        name='activity_dashboard_feed'),
 )
