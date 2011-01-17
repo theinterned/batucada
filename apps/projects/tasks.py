4ddf6e84aef6d2c6c7e55d5eeb5c3bcf877411a4
Paul Osman <paul@eval.ca>
2011-01-16 19:30:21 -0500

    Try as I may, I just can't make this ugly task pretty.

--- apps/projects/tasks.py
+++ apps/projects/tasks.py
@@ -1,5 +1,5 @@
 import os
-import time
+import glob
 import logging
 import random
 
@@ -22,33 +22,65 @@ class ThumbnailGenerator(Task):
             'filename': filename,
         }
 
-    def run(self, media):
-        filename = os.path.join(settings.MEDIA_ROOT, media.project_file.name)
-        ffmpeg = getattr(settings, 'FFMPEG_PATH', None)
-        if not ffmpeg:
-            log.warn("No ffmpeg path set, nothing to do.")
-            return
-        framemask = "frame" + str(time.time()) + ".%d.jpg"
-        cmd = '%s -y -an -vframes 10 -r 1 -i %s %s -v 1 > /dev/null 2>&1' % (
-            ffmpeg, filename, framemask)
-        log.debug("Command: %s" % (cmd,))
-        os.chdir("/tmp")
-        ret = os.system(cmd)
-        if ret != 0:
-            log.warn("ffmpeg returned non-zero: %d" % (ret,))
-            return
-        image = framemask % (random.choice(range(1, 10)),)
+    def create_frames(self, media):
+        """
+        Using ffmpeg, extract one image per frame for the first n frames
+        of a video file, where n is specified by the ``FFMPEG_VFRAMES``
+        configuration variable.
+        """
+        working_dir = getattr(settings, 'FFMPEG_WD', '/tmp')
+        abs_path = os.path.join(settings.MEDIA_ROOT, media.project_file.name)
+        framemask = self.frame_prefix + ".%d.jpg"
+        cmd = '%s -y -an -vframes %d -r 1 -i %s %s -v 1 > /dev/null 2>&1' % (
+            self.ffmpeg, self.vframes, abs_path, framemask)
+        os.chdir(working_dir)
+        return_value = os.system(cmd)
+        log.debug("Running command: %s" % (cmd,))
+        if return_value != 0:
+            log.warn("ffmpeg returned non-zero: %d" % (return_value,))
+            return False
+        return True
+
+    def create_thumbnail(self, media):
+        """
+        Select a random frame from the video to use as the video thumbnail.
+        """
+        image = "%s.%d.jpg" % (self.frame_prefix,
+                               random.choice(range(1, self.vframes + 1)),)
         if not os.path.exists(image):
             log.warn("File %s does not exist!" % (image,))
             return
-        thumbnail, ext = os.path.splitext(os.path.basename(filename))
+        thumbnail, ext = os.path.splitext(os.path.basename(image))
         thumbnail_filename = self.determine_path(
             media.project, "%s_thumbnail.png" % (thumbnail,))
         media.thumbnail = thumbnail_filename
         media.save()
-        abs_thumbnail = os.path.join(settings.MEDIA_ROOT, thumbnail_filename)
-        log.debug("Abs path: %s" % (abs_thumbnail,))
+        abs_path = os.path.join(settings.MEDIA_ROOT, thumbnail_filename)
         im = Image.open(image)
         im.thumbnail((128, 128), Image.ANTIALIAS)
-        im.save(abs_thumbnail, 'PNG')
-        #im.save(thumbnail_filename, "PNG")
+        im.save(abs_path, 'PNG')
+        return True
+
+    def run(self, media):
+
+        self.ffmpeg = getattr(settings, 'FFMPEG_PATH', None)
+        self.vframes = getattr(settings, 'FFMPEG_VFRAMES', 10)
+
+        if not self.ffmpeg:
+            log.warn("No ffmpeg path set. Nothing to do.")
+            return
+
+        self.frame_prefix = "frame%d_%d" % (media.project.id, media.id)
+
+        if not self.create_frames(media):
+            log.warn("Error creating frames.")
+            return
+
+        if not self.create_thumbnail(media):
+            log.warn("Error creating thumbnail")
+            return
+
+        # remove frame image files.
+        files = glob.glob(self.frame_prefix + '*')
+        for f in files:
+            os.unlink(f)
