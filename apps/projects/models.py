8ee7f8df24ea8d45752e26ebaf8cafb2a74b1705
Paul Osman <paul@eval.ca>
2011-01-22 17:58:00 -0500

    Simplifying thumbnail_or_default and image_or_default

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -120,11 +120,9 @@ class ProjectMedia(ModelBase):
                                   storage=storage.ImageStorage())
 
     def thumbnail_or_default(self):
-      """Return project media's thumbnail or a default."""
-      if self.thumbnail:
-          return self.thumbnail
-      else:
-          return 'images/file-default.png'                                                                    
+        """Return project media's thumbnail or a default."""
+        return self.thumbnail or 'images/file-default.png'
+
 
 ###########
 # Signals #
