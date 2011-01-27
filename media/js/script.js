5047093d0bc614e6359dea8bcbcaab740f528ab6
Paul Osman <paul@eval.ca>
2011-01-26 23:01:45 -0500

    Add javascript for recaptcha alternate image / audio links

--- media/js/script.js
+++ media/js/script.js
@@ -220,14 +220,20 @@ $(document).ready(function() {
     });
 });
 
+// Recaptcha
+var RecaptchaOptions = { theme : 'custom' };
 
+$('#recaptcha_different').click(function(e) {
+    e.preventDefault();
+    Recaptcha.reload();
+});
 
+$('#recaptcha_audio').click(function(e) {
+    e.preventDefault();
+    Recaptcha.switch_type('audio');
+});
 
-
-
-
-
-
-
-
-
+$('#recaptcha_help').click(function(e) {
+    e.preventDefault();
+    Recaptcha.showhelp();
+});
