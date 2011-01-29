5724efbcd05adecdfc734ae99b231d24cad4e554
Paul Osman <paul@eval.ca>
2011-01-29 17:12:18 -0500

    Submit form data on POST

--- media/js/script.js
+++ media/js/script.js
@@ -206,12 +206,13 @@ jQuery.fn.tabLinks = function(element) {
         
         $modal.find('.tabs a').each(function() {
             log(this);
-	    var tab = $(this).getOwnTab();
+            var tab = $(this).getOwnTab();
             if (tab) {
                 var $form = $(tab).find('form');
                 $.ajax({
                     type: $form.attr('method'),
                     url: $form.attr('action'),
+                    data: $form.serialize(),
                     success: function(data) {
                         log(data);
                     }
