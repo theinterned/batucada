1f57474f4c93044134767aac81ef5f8b91248460
Paul Osman <paul@eval.ca>
2011-02-10 16:57:09 -0500

    Support legacy usernames that have a '.' char in them

--- apps/activity/urls.py
+++ apps/activity/urls.py
@@ -6,7 +6,7 @@ from activity import feeds
 urlpatterns = patterns('',
     url(r'^activity/(?P<activity_id>[\d]+)/$', 'activity.views.index',
         name='activity_index'),
-    url(r'^(?P<username>[\w\- ]+)/feed$', feeds.UserActivityFeed(),
+    url(r'^(?P<username>[\w\-\. ]+)/feed$', feeds.UserActivityFeed(),
         name='activity_user_feed'),
     url(r'^projects/(?P<project>[\w\- ]+)/feed$', feeds.ProjectActivityFeed(),
         name='activity_project_feed'),
