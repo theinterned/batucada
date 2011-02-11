78267895cbe89f8d26a0d7ad0ef8d7efb6e462cf
James Walker <walkah@walkah.net>
2011-02-11 09:51:28 -0500

    manager with 'active' challenges

--- apps/challenges/models.py
+++ apps/challenges/models.py
@@ -12,6 +12,13 @@ import caching.base
 
 log = logging.getLogger(__name__)
 
+class ChallengeManager(caching.base.CachingManager):
+    def active(self, project_id=0):
+        q = Challenge.objects.filter(start_date__lte=datetime.now()).filter(end_date__gte=datetime.now())
+        if project_id:
+            q = q.filter(id=project_id)
+        return q
+
 class Challenge(ModelBase):
     """ Inovation (design) Challenges """
     title = models.CharField(max_length=100, unique=True)
@@ -28,6 +35,8 @@ class Challenge(ModelBase):
                                    related_name='challenges')
 
     is_open = models.BooleanField()
+
+    objects = ChallengeManager()
 
     def save(self):
         """Make sure each challenge has a unique slug."""
