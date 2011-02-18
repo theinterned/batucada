e75a9215cdc7fd37f1203c05bb806bdb3047779d
Paul Osman <paul@eval.ca>
2011-02-18 10:56:18 -0500

    Removing obseleted test

--- apps/drumbeatmail/tests.py
+++ apps/drumbeatmail/tests.py
@@ -27,15 +27,6 @@ class TestDrumbeatMail(test_utils.TestCase):
         self.user_two.save()
         self.user_two.create_django_user()
 
-    def test_messaging_user_not_following(self):
-        form = ComposeForm(data={
-            'recipient': self.user_two,
-            'subject': 'Foo',
-            'body': 'Bar',
-        }, sender=self.user)
-        self.assertTrue(form.is_bound)
-        self.assertFalse(form.is_valid())
-
     def test_messaging_user_following(self):
         print "From test: %s" % (self.user.user,)
         print "From test: %s" % (self.user_two.user,)
