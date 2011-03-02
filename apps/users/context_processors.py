57b391c409c35988ec598a8846093d0409d2653f
James Walker <walkah@walkah.net>
2011-03-02 17:28:32 -0500

    fix  bug #632438 - add a context_processor to give a login url with a next= param.

--- apps/users/context_processors.py
+++ apps/users/context_processors.py
@@ -1,5 +1,8 @@
-from messages.models import Message
+from django.utils.http import urlquote
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
 
+from messages.models import Message
 
 def messages(request):
     if request.user.is_authenticated():
@@ -9,3 +12,9 @@ def messages(request):
         return {'preview_messages': messages}
     else:
         return {}
+
+def login_with_redirect(request):
+    login_url = settings.LOGIN_URL
+    path = urlquote(request.get_full_path())
+    url = '%s?%s=%s' % (settings.LOGIN_URL, REDIRECT_FIELD_NAME, path)
+    return {'login_url': url}    
