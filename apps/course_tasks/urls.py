f705dda8720636672b61f097200a893d36ca0701
Zuzel Vera <zuzel.vp@gmail.com>
2011-03-09 09:46:49 -0500

    Renaming todos app to course_tasks.
    * The original idea behind this app is that "Courses should support tasks with an optional due date."
    * Also updating migrations.

--- apps/course_tasks/urls.py
+++ apps/course_tasks/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-  url(r'^(?P<todo_id>\d+)/$', 'todos.views.show',
+urlpatterns = patterns('course_tasks.views',
+  url(r'^(?P<todo_id>\d+)/$', 'show',
       name='todos_show'),
-  url(r'^create/$', 'todos.views.create',
+  url(r'^create/$', 'create',
       name='todos_create'),
   url(r'^create/project/(?P<project_id>\d+)/$',
-      'todos.views.create_project_todo',
+      'create_project_todo',
       name='todos_create_project'),
 )
