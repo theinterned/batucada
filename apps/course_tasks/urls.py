2cd05126084953f66bb28348e950633f5a6cfa48
Zuzel Vera <zuzel.vp@gmail.com>
2011-03-09 12:30:25 -0500

    Relocating "Post Task" form.
    * Now users can post tasks for a project or for themselves.
    * Cleanup message followers template.
    * More renaming in the ex-todos app.
    * TODO: Add datetime widget.
    * TODO: Clean show view.
    * TODO: Improve tasks display.
    * TODO: Filter project/user pages by tasks.

--- apps/course_tasks/urls.py
+++ apps/course_tasks/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('course_tasks.views',
-  url(r'^(?P<todo_id>\d+)/$', 'show',
-      name='todos_show'),
+  url(r'^(?P<task_id>\d+)/$', 'show',
+      name='task_show'),
   url(r'^create/$', 'create',
-      name='todos_create'),
+      name='create_task'),
   url(r'^create/project/(?P<project_id>\d+)/$',
-      'create_project_todo',
-      name='todos_create_project'),
+      'create_project_task',
+      name='create_project_task'),
 )
--- apps/course_tasks/views.py
+++ apps/course_tasks/views.py
@@ -6,8 +6,8 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.template import RequestContext
 from django.utils.translation import ugettext as _
 
-from course_tasks.forms import TodoForm
-from course_tasks.models import Todo
+from course_tasks.forms import TaskForm
+from course_tasks.models import CourseTask
 from projects.models import Project
 from users.decorators import login_required
 
@@ -16,46 +16,56 @@ from drumbeat import messages
 log = logging.getLogger(__name__)
 
 
-def show(request, todo_id):
-    todo = get_object_or_404(Todo, id=todo_id)
-    return render_to_response('todos/show.html', {
-        'todo': todo,
+def show(request, task_id):
+    task = get_object_or_404(CourseTask, id=task_id)
+    return render_to_response('course_tasks/show.html', {
+        'task': task,
     }, context_instance=RequestContext(request))
 
 
 @login_required
 def create(request):
-    if request.method != 'POST' or 'todo' not in request.POST:
-        return HttpResponseRedirect('/')
-    form = TodoForm(data=request.POST)
-    if form.is_valid():
-        todo = form.save(commit=False)
-        todo.author = request.user.get_profile()
-        todo.save()
+    profile = request.user.get_profile()
+    if request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = profile
+            task.save()
+            return HttpResponseRedirect(reverse('dashboard_index'))
+        else:
+            log.debug("form error: %s" % (str(form.errors)))
+            messages.error(request, _('There was an error posting '
+                                      'your course task'))
     else:
-        log.debug("form error: %s" % (str(form.errors)))
-        messages.error(request, _('There was an error posting '
-                                  'your todo update'))
-    return HttpResponseRedirect('/')
-
+        form = TaskForm()
+    return render_to_response('course_tasks/post_task.html', {
+        'form': form,
+    }, context_instance=RequestContext(request))
 
 @login_required
-def create_project_todo(request, project_id):
-    if request.method != 'POST':
-        return HttpResponseRedirect('/')
+def create_project_task(request, project_id):
     project = get_object_or_404(Project, id=project_id)
     profile = request.user.get_profile()
-    form = TodoForm(data=request.POST)
-    if form.is_valid():
-        todo = form.save(commit=False)
-        todo.author = request.user.get_profile()
-        todo.project = project
-        todo.save()
-        log.debug("Saved todo by user (%d) to project (%d): %s" % (
-            profile.id, project.id, todo))
+    if request.method == 'POST':
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.author = request.user.get_profile()
+            task.project = project
+            task.save()
+            log.debug("Saved task by user (%d) to course (%d): %s" % (
+                profile.id, project.id, task))
+            return HttpResponseRedirect(reverse('projects_show',
+                kwargs=dict(slug=project.slug)))
+        else:
+            log.debug("form error: %s" % (str(form.errors)))
+            messages.error(request, _('There was an error posting '
+                                      'your course task'))
     else:
-        log.debug("form error: %s" % (str(form.errors)))
-        messages.error(request, _('There was an error posting '
-                                  'your todo update'))
-    return HttpResponseRedirect(
-        reverse('projects_show', kwargs=dict(slug=project.slug)))
+        form = TaskForm()
+        form.fields['project'].initial = project.pk
+    return render_to_response('course_tasks/post_task.html', {
+        'form': form,
+        'project': project,
+    }, context_instance=RequestContext(request))
