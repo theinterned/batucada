c4dca2e9d41f3cf35b314f567c0ad88112d8fc19
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-04-07 15:58:16 -0700

    Pulled out constants and put them in settings.py

--- apps/users/models.py
+++ apps/users/models.py
@@ -6,6 +6,7 @@ import string
 import hashlib
 import os
 
+from django.conf import settings
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
@@ -25,28 +26,6 @@ from projects.models import Project
 from users import tasks
 
 import caching.base
-
-TAGS = ('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'b', 'em', 'i', 'strong',
-        'ol', 'ul', 'li', 'hr', 'blockquote', 'p',
-        'span', 'pre', 'code', 'img',
-        'u', 'strike', 'sub', 'sup', 'address', 'div',
-        'table', 'thead', 'tr', 'th', 'caption', 'tbody', 'td', 'br')
-
-ALLOWED_ATTRIBUTES = {
-    'a': ['href', 'title'],
-    'img': ['src', 'alt', 'style', 'title'],
-    'p': ['style'],
-    'table': ['align', 'border', 'cellpadding', 'cellspacing',
-        'style', 'summary'],
-    'th': ['scope'],
-    'span': ['style'],
-    'pre': ['class'],
-    'code': ['class'],
-}
-
-ALLOWED_STYLES = ['text-align', 'margin-left', 'border-width',
-    'border-style', 'margin', 'float', 'width', 'height',
-    'font-family', 'font-size', 'color', 'background-color']
 
 log = logging.getLogger(__name__)
 
@@ -221,8 +200,8 @@ def clean_html(sender, **kwargs):
         log.debug("Cleaning html.")
         if instance.bio:
             instance.bio = bleach.clean(instance.bio,
-                tags=TAGS, attributes=ALLOWED_ATTRIBUTES,
-                styles=ALLOWED_STYLES)
+                tags=settings.TAGS, attributes=settings.ALLOWED_ATTRIBUTES,
+                styles=settings.ALLOWED_STYLES)
 
 pre_save.connect(clean_html, sender=UserProfile) 
 
