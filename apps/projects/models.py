2bd2bf7b758973c2cfc65dd321abab3724285728
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-04-07 15:58:16 -0700

    Pulled out constants and put them in settings.py

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -26,30 +26,7 @@ from activity.models import Activity
 from projects.tasks import ThumbnailGenerator
 
 import caching.base
-
-
-TAGS = ('h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'a', 'b', 'em', 'i', 'strong',
-        'ol', 'ul', 'li', 'hr', 'blockquote', 'p',
-        'span', 'pre', 'code', 'img',
-        'u', 'strike', 'sub', 'sup', 'address', 'div',
-        'table', 'thead', 'tr', 'th', 'caption', 'tbody', 'td', 'br')
-
-ALLOWED_ATTRIBUTES = {
-    'a': ['href', 'title'],
-    'img': ['src', 'alt', 'style', 'title'],
-    'p': ['style'],
-    'table': ['align', 'border', 'cellpadding', 'cellspacing',
-        'style', 'summary'],
-    'th': ['scope'],
-    'span': ['style'],
-    'pre': ['class'],
-    'code': ['class'],
-}
-
-ALLOWED_STYLES = ['text-align', 'margin-left', 'border-width',
-    'border-style', 'margin', 'float', 'width', 'height',
-    'font-family', 'font-size', 'color', 'background-color']
-
+ 
 log = logging.getLogger(__name__)
 
 
@@ -240,12 +217,12 @@ def clean_html(sender, **kwargs):
         log.debug("Cleaning html.")
         if instance.short_description:
             instance.short_description = bleach.clean(instance.short_description,
-                tags=TAGS, attributes=ALLOWED_ATTRIBUTES,
-                styles=ALLOWED_STYLES)
+                tags=settings.TAGS, attributes=settings.ALLOWED_ATTRIBUTES,
+                styles=settings.ALLOWED_STYLES)
         if instance.long_description:
             instance.long_description = bleach.clean(instance.long_description,
-                tags=TAGS, attributes=ALLOWED_ATTRIBUTES,
-                styles=ALLOWED_STYLES)
+                tags=settings.TAGS, attributes=settings.ALLOWED_ATTRIBUTES,
+                styles=settings.ALLOWED_STYLES)
 
 pre_save.connect(clean_html, sender=Project) 
 
