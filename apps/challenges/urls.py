a9548d31e5040d5c728f91689dc6db043a915b14
Paul Osman <paul@eval.ca>
2011-04-14 17:53:06 -0400

    Add project challenges feed. Use challenge brief for description and restrict results to active challenges

--- apps/challenges/urls.py
+++ apps/challenges/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 from models import Submission
-from challenges.feeds import ChallengesFeed
+from challenges.feeds import ChallengesFeed, ProjectChallengesFeed
 from voting.views import vote_on_object
 
 vote_dict = {
@@ -20,6 +20,8 @@ urlpatterns = patterns('',
       name='challenges_edit_image'),
   url(r'^(?P<slug>[\w-]+)/edit/image/async$', 'challenges.views.edit_challenge_image_async',
       name='challenges_edit_image_async'),
+  url(r'^(?P<project>[\w-]+)/feed/$', ProjectChallengesFeed(),
+      name='challenges_project_feed'),
   url(r'^feed/$', ChallengesFeed(), name='challenges_feed'),
 
   url(r'^(?P<slug>[\w-]+)/$', 'challenges.views.show_challenge',
