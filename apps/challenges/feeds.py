316d16e9e2b36baa931a9cf975ef1d7d01f0de56
Paul Osman <paul@eval.ca>
2011-04-14 17:53:06 -0400

    Add project challenges feed. Use challenge brief for description and restrict results to active challenges

--- apps/challenges/feeds.py
+++ apps/challenges/feeds.py
@@ -1,6 +1,8 @@
 from django.core.urlresolvers import reverse
+from django.shortcuts import get_object_or_404
 from django_push.publisher.feeds import Feed, HubAtom1Feed
 
+from projects.models import Project
 from challenges.models import Challenge
 
 
@@ -8,7 +10,20 @@ class ChallengesFeed(Feed):
     feed_type = HubAtom1Feed
 
     def items(self):
-        return Challenge.objects.all().order_by('-created_on')
+        return Challenge.objects.active().order_by('-created_on')
 
     def link(self):
         return reverse('challenges_feed')
+
+    def item_description(self, item):
+        return item.brief
+
+
+class ProjectChallengesFeed(ChallengesFeed):
+
+    def get_object(self, request, project):
+        return get_object_or_404(Project, slug=project)
+
+    def items(self, project):
+        return Challenge.objects.active().filter(project=project).order_by(
+            '-created_on')
