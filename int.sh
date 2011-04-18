4920121fe593200efffaf499bb9ba48dca31062d
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2011-04-18 03:17:46 -0500

    python manage instead of django-admin.py

--- int.sh
+++ int.sh
@@ -1,10 +1,10 @@
 # Prepares existing .po for localizations and compiles them
 # Public domain
 # To make first time localization (for example for de_DE) run
-#     django-admin.py makemessages -e ".html,.txt" -l de_DE
+#     python manage.py makemessages -e ".html,.txt" -l de_DE
 
 l=`(cd locale; ls)`
 for lang in $l; do
-	django-admin.py makemessages -e ".html,.txt" -l $lang
+	python manage.py makemessages -e ".html,.txt" -l $lang
 done;
-django-admin.py compilemessages
+python manage.py compilemessages
