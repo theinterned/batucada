bc56417d57c91c98803ba664ef5e9af384259931
James Walker <walkah@walkah.net>
2011-05-07 12:34:47 -0400

    allow time editing for start/end date (to do 11:59pm)

--- apps/challenges/forms.py
+++ apps/challenges/forms.py
@@ -2,7 +2,6 @@ import logging
 
 from django import forms
 from django.conf import settings
-from django.forms.extras.widgets import SelectDateWidget
 from django.utils.translation import ugettext_lazy as _
 
 from challenges.models import (Challenge, Submission, Judge,
@@ -17,10 +16,6 @@ class ChallengeForm(forms.ModelForm):
     class Meta:
         model = Challenge
         exclude = ('slug', 'project', 'created_by', 'created_on', 'is_open')
-        widgets = {
-            'start_date': SelectDateWidget(),
-            'end_date': SelectDateWidget(),
-        }
 
 
 class ChallengeImageForm(forms.ModelForm):
