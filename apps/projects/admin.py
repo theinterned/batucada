ae9969db395b1d4b4af55c45c5f2c3aa1ebddd5b
Zuzel Vera <zuzel.vp@gmail.com>
2011-05-17 19:00:31 -0400

    Providing more functionality for the study groups list on the admin interface.

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from projects.models import Project
 
 
-admin.site.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    list_display = ('name', 'created_on', 'school', 'featured',
+        'under_development', 'not_listed', 'signup_closed', 
+        'archived')
+    list_filter = list_display[1:]
+    search_fields = ('name',)
+
+
+admin.site.register(Project, ProjectAdmin)
+
