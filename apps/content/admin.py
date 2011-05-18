05440316f326fdf587da32e283424ef41c6fe45f
Zuzel Vera <zuzel.vp@gmail.com>
2011-05-17 23:41:28 -0400

    Enabling more functionality for study groups content on the admin site.

--- apps/content/admin.py
+++ apps/content/admin.py
@@ -1,7 +1,33 @@
 from django.contrib import admin
+from django.utils.translation import ugettext as _
+
 from content.models import Page, PageVersion, PageComment
 
 
-admin.site.register(Page)
-admin.site.register(PageVersion)
-admin.site.register(PageComment)
+class PageAdmin(admin.ModelAdmin):
+    date_hierarchy = 'last_update'
+    list_display = ('id', 'title', 'author', 'project', 'index', 'last_update',
+        'listed', 'collaborative', 'editable', 'deleted')
+    list_filter = list_display[3:]
+    search_fields = ('id', 'title', 'slug', 'project__slug', 'project__name',
+        'author__username', 'author__full_name')
+
+
+class PageVersionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'date'
+    list_display = ('id', 'page', 'author', 'date', 'deleted')
+    list_filter = list_display[3:]
+    search_fields = ('id', 'page__slug', 'page__title', 'author__username', 'author__full_name')
+
+
+class PageCommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_on'
+    list_display = ('id', 'page', 'author', 'created_on', 'deleted')
+    list_filter = list_display[3:]
+    search_fields = ('id', 'page__slug', 'page__title', 'author__username', 'author__full_name')
+    
+
+
+admin.site.register(Page, PageAdmin)
+admin.site.register(PageVersion, PageVersionAdmin)
+admin.site.register(PageComment, PageCommentAdmin)
--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -4,18 +4,19 @@ from projects.models import Project, Participation
 
 class ProjectAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_on'
-    list_display = ('name', 'created_on', 'school', 'featured',
+    list_display = ('id', 'name', 'created_on', 'school', 'featured',
         'under_development', 'not_listed', 'signup_closed', 
         'archived')
-    list_filter = list_display[1:]
-    search_fields = ('name', 'slug', 'school__name')
+    list_filter = list_display[2:]
+    search_fields = ('id', 'name', 'slug', 'school__name')
 
 class ParticipationAdmin(admin.ModelAdmin):
     date_hierarchy = 'joined_on'
-    list_display = ('user', 'project', 'organizing', 'joined_on',
+    list_display = ('id', 'user', 'project', 'organizing', 'joined_on',
         'left_on', 'no_wall_updates', 'no_updates')
-    list_filter = list_display[2:]
-    search_fields = ('project__name', 'user__username', 'user__full_name', 'project__slug')
+    list_filter = list_display[3:]
+    search_fields = ('id', 'project__name', 'user__username', 'user__full_name',
+        'project__slug')
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Participation, ParticipationAdmin)
