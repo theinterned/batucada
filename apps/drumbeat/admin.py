f735cf8ccd6d040ee3a06aa181de69f2523ac0bc
Zuzel Vera <zuzel.vp@gmail.com>
2011-05-18 07:26:02 -0400

    Enabling more functionality for relationships and schools on the admin site.

--- apps/drumbeat/admin.py
+++ apps/drumbeat/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group, User
 from django.contrib.comments.models import Comment
+from django.contrib.sites.models import Site
 
 from django_openid_auth.models import Nonce, Association, UserOpenID
-
 from djcelery.models import TaskState, PeriodicTask, CrontabSchedule, IntervalSchedule, WorkerState
 from taggit.models import Tag
 from wellknown.models import Resource
@@ -17,13 +17,13 @@ print admin.site._registry
 
 admin.site.unregister([Group, User, Comment, Nonce, Association, UserOpenID,
     TaskState, PeriodicTask, CrontabSchedule, IntervalSchedule, WorkerState,
-    Tag, Resource, Vote])
+    Tag, Resource, Vote, Site])
 
 
 admin.site._registry[Message].date_hierarchy = 'sent_at'
 admin.site._registry[Message].list_display = ('id', 'subject', 'sender', 'recipient', 'sent_at')
 admin.site._registry[Message].list_filter = ('sent_at',)
-admin.site._registry[Message].search_fields = ('id', 'subject', 'body', 'sender', 'recipient')
+admin.site._registry[Message].search_fields = ('id', 'subject', 'body', 'sender__username', 'recipient__username')
 
 
 
