2249542c42f593d47b2b6d0b218df9458e8e3ba9
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-08-21 17:22:37 -0400

    Narrowed what's tracked to groups

--- lernanta/apps/tracker/models.py
+++ lernanta/apps/tracker/models.py
@@ -1,20 +1,16 @@
-
 from django.contrib.auth.models import User
 from django.contrib.sessions.models import Session
 from django.db import models
 
-class Visitor(models.Model):
-    session = models.OneToOneField(Session, db_index=True,null=True,blank=True,primary_key=True)
+
+class PageView(models.Model):
+    session_key = models.CharField(max_length=100)
     user = models.ForeignKey(User, null=True, blank=True)
-    session_start = models.DateTimeField(help_text="Date Request started processing", auto_now_add=True, db_index=True)
-    #stats = VisitorActivityManager()
-    request_url = models.CharField(max_length=755,db_index=True)
+    access_time = models.DateTimeField(auto_now_add=True, db_index=True)
+    request_url = models.CharField(max_length=755, db_index=True)
     referrer_url = models.URLField(
                          verify_exists=False,
                          db_index=True,
                          blank=True, null=True)
     ip_address = models.IPAddressField(
-                         blank=True,null=True)
-
-
-# VisitorManager?
+                         blank=True, null=True)
