58c21f812d946c4409176f2b2865d2a14b7dc37d
Zuzel Vera <zuzel.vp@gmail.com>
2011-08-25 17:17:13 -0400

    ASCIIMathML support for UCIrvine courses (and any math group/course on p2pu).
    * http://www.wjagray.co.uk/maths/ASCIIMathTutorial.html

--- lernanta/apps/richtext/templatetags/embed.py
+++ lernanta/apps/richtext/templatetags/embed.py
@@ -51,6 +51,6 @@ def embed(html):
     """Finds Slideshare [slideshare: ...] tags, and replace them
     with an embeddable HTML snippet.
     """
-    return re.sub(EMBED_RE, replace, html)
+    return '<div class="richtext_section">%s</div>' % re.sub(EMBED_RE, replace, html)
 
 embed.is_safe = True  # Don't escape HTML
