c9e4ffc4095775e96a8ba0929d718f7baa7179dd
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-08-28 03:11:46 -0700

    Added time on page

--- lernanta/apps/tracker/models.py
+++ lernanta/apps/tracker/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class PageView(models.Model):
-    session_key = models.CharField(max_length=100)
+    session_key = models.CharField(max_length=100, db_index=True)
     user = models.ForeignKey(User, null=True, blank=True)
     access_time = models.DateTimeField(auto_now_add=True, db_index=True)
     request_url = models.CharField(max_length=755, db_index=True)
@@ -14,3 +14,8 @@ class PageView(models.Model):
                          blank=True, null=True)
     ip_address = models.IPAddressField(
                          blank=True, null=True)
+    time_on_page = models.IntegerField(blank=True, null=True)
+
+    def __repr__(self):
+        return '<Session: key %s, access time %s, request_url %s, time_on_page %s, user %s>' \
+                % (self.session_key, self.access_time, self.request_url, self.time_on_page, self.user)
