337ae4c528d36a3421c453a78a0bf09aa69dbab5
Zuzel Vera <zuzel.vp@gmail.com>
2011-09-13 20:28:29 -0400

    Changing indentation and moving bot detection out of the for (please review).

--- lernanta/apps/tracker/middleware.py
+++ lernanta/apps/tracker/middleware.py
@@ -14,34 +14,33 @@ class PageViewTrackerMiddleware:
     def process_request(self, request):
         BotNames=['Googlebot','Slurp','Twiceler','msnbot','KaloogaBot','YodaoBot','"Baiduspider','googlebot','Speedy Spider','DotBot']
         user_agent=request.META.get('HTTP_USER_AGENT', None)
-        print user_agent
+        for botname in BotNames:
+            if botname in user_agent:
+                # do not save the page view if the visitor is a known bot
+                return
+        
         # ensure that the request.path begins with any of the prefixes
         for prefix in settings.TRACKING_PREFIXES:
-
-            for botname in BotNames:
-                if botname in user_agent:
-                    # do not save the page view if the visitor is a known bot
-                    return
-                if fnmatch.fnmatch(request.path, prefix):
-                    ip_address = utils.get_ip(request)
-                    pageview = PageView()
-                    pageview.session_key = request.session.session_key
-                    pageview.ip_address = ip_address
-                    pageview.request_url = request.path
-                    pageview.referrer_url = utils.u_clean(request.META.get('HTTP_REFERER', 'unknown')[:255])
-    
-                    try:
-                        oldpageview = PageView.objects.filter(session_key=request.session.session_key).order_by('-access_time')[0]
-                    except IndexError, error:
-                        pass
-                    else:
-                        time_on_page = (datetime.datetime.now() - oldpageview.access_time)
-                        if time_on_page.seconds < 3600 and time_on_page.days == 0:
-                            oldpageview.time_on_page = time_on_page.seconds
-                            oldpageview.save()
-    
+            if fnmatch.fnmatch(request.path, prefix):
+                ip_address = utils.get_ip(request)
+                pageview = PageView()
+                pageview.session_key = request.session.session_key
+                pageview.ip_address = ip_address
+                pageview.request_url = request.path
+                pageview.referrer_url = utils.u_clean(request.META.get('HTTP_REFERER', 'unknown')[:255])
+                try:
+                    oldpageview = PageView.objects.filter(session_key=request.session.session_key).order_by('-access_time')[0]
+                except IndexError, error:
+                    pass
                 else:
-                    return
+                    time_on_page = (datetime.datetime.now() - oldpageview.access_time)
+                    if time_on_page.seconds < 3600 and time_on_page.days == 0:
+                        oldpageview.time_on_page = time_on_page.seconds
+                        oldpageview.save()
+                break
+        else:
+            # it did not find any matching prefixes.
+            return
 
         if request.user.is_authenticated():
             now = datetime.datetime.now()
