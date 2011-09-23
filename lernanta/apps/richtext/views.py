d8c8a6ca7ad99b73454bebf662a5c72e1dbfe7d6
Zuzel Vera <zuzel.vp@gmail.com>
2011-09-23 12:57:23 -0400

    Restricting file uploads.

--- lernanta/apps/richtext/views.py
+++ lernanta/apps/richtext/views.py
@@ -6,9 +6,9 @@ from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from django.utils.translation import ugettext as _
 
-from ckeditor.views import upload as ckeditor_upload
-from ckeditor.views import get_available_name, get_media_url
+from ckeditor.views import get_available_name, get_media_url, create_thumbnail
 
 from richtext.forms import FileBrowser
 
@@ -16,9 +16,7 @@ from richtext.forms import FileBrowser
 @csrf_exempt
 def upload_image(request):
     # Allow the view to callback ckeditor's dialog.
-    response = ckeditor_upload(request)
-    response['X-Frame-Options'] = 'SAMEORIGIN'
-    return response
+    return upload_file(request, image_upload=True)
 
 
 def get_file_browse_urls(user=None):
@@ -69,7 +67,7 @@ def browse_file(request):
     return render_to_response('richtext/browse.html', context)
 
 
-def get_upload_filename(upload_name, user):
+def get_upload_filename(upload_name, user, image_upload=False):
     # If CKEDITOR_RESTRICT_BY_USER is True upload file to user specific path.
     if getattr(settings, 'CKEDITOR_RESTRICT_BY_USER', False):
         user_path = user.username
@@ -80,7 +78,11 @@ def get_upload_filename(upload_name, user):
     date_path = datetime.now().strftime('%Y/%m/%d')
 
     # Complete upload path (upload_path + date_path).
-    upload_path = os.path.join(settings.CKEDITOR_FILE_UPLOAD_PATH, user_path, date_path)
+    if image_upload:
+        uploads_dir = settings.CKEDITOR_UPLOAD_PATH
+    else:
+        uploads_dir = settings.CKEDITOR_FILE_UPLOAD_PATH
+    upload_path = os.path.join(uploads_dir, user_path, date_path)
 
     # Make sure upload_path exists.
     if not os.path.exists(upload_path):
@@ -91,24 +93,44 @@ def get_upload_filename(upload_name, user):
 
 
 @csrf_exempt
-def upload_file(request):
+def upload_file(request, image_upload=False):
     # Get the uploaded file from request.
     upload = request.FILES['upload']
+    func_num = request.GET['CKEditorFuncNum']
 
-    # Open output file in which to store upload.
-    upload_filename = get_upload_filename(upload.name, request.user)
-    out = open(upload_filename, 'wb+')
+    upload_ext = os.path.splitext(upload.name)[1].lower()
+    if image_upload:
+        valid_extensions = settings.CKEDITOR_IMAGE_UPLOAD_EXTENSIONS
+    else:
+        valid_extensions = settings.CKEDITOR_FILE_UPLOAD_EXTENSIONS
 
-    # Iterate through chunks and write to destination.
-    for chunk in upload.chunks():
-        out.write(chunk)
-    out.close()
+    if upload_ext in valid_extensions:
+        # Open output file in which to store upload.
+        upload_filename = get_upload_filename(upload.name, request.user, image_upload)
 
-    # Respond with Javascript sending ckeditor upload url.
-    url = get_media_url(upload_filename)
-    response = HttpResponse("""
-        <script type='text/javascript'>
-            window.parent.CKEDITOR.tools.callFunction(%s, '%s');
-        </script>""" % (request.GET['CKEditorFuncNum'], url))
+        out = open(upload_filename, 'wb+')
+
+        # Iterate through chunks and write to destination.
+        for chunk in upload.chunks():
+            out.write(chunk)
+        out.close()
+
+        if image_upload:
+            create_thumbnail(upload_filename)
+
+        # Respond with Javascript sending ckeditor upload url.
+        url = get_media_url(upload_filename)
+        response = HttpResponse("""
+            <script type='text/javascript'>
+                window.parent.CKEDITOR.tools.callFunction(%s, '%s');
+            </script>""" % (func_num, url))
+    else:
+        # Respond with JavaScript sending error msg to ckeditor dialog.
+        msg = _('Extension %(ext)s is not allowed. Only %(allowed)s are allowed.')
+        kwargs = {'ext': upload_ext, 'allowed': ", ".join(valid_extensions)}
+        response = HttpResponse("""
+            <script type='text/javascript'>
+                window.parent.CKEDITOR.tools.callFunction(%s, '', '%s');
+            </script>""" % (func_num, msg % kwargs))
     response['X-Frame-Options'] = 'SAMEORIGIN'
     return response
