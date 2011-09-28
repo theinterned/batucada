6fd90679fae2f5683b3b80a88af43e6fc11eb53d
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-09-27 22:15:16 -0400

    Moved logic to its own model.

--- lernanta/apps/badges/forms.py
+++ lernanta/apps/badges/forms.py
@@ -2,11 +2,12 @@ from django import forms
 
 from badges.models import Badge
 
+
 class BadgeForm(forms.ModelForm):
 
     class Meta:
         model = Badge
-        fields = ('name', 'description', 'image', 
+        fields = ('name', 'description', 'image',
             'assessment_type', 'badge_type', 'rubrics')
         widgets = {
             'assessment_type': forms.RadioSelect,
