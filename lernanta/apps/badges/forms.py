ae37287bd488f09f86869512bce2e23a36e0db62
Jessica Ledbetter <jessica.ledbetter@gmail.com>
2011-10-09 13:49:10 -0700

    in progress. Can see the rubrics and a score but still no input form.

--- lernanta/apps/badges/forms.py
+++ lernanta/apps/badges/forms.py
@@ -1,5 +1,9 @@
 from django import forms
+from django.db import models
 from django.forms.models import inlineformset_factory
+from django.utils.translation import ugettext as _
+
+from richtext.models import RichTextField
 
 from badges.models import Badge, Submission, Assessment, Rating
 
@@ -23,11 +27,33 @@ class SubmissionForm(forms.ModelForm):
         fields = ('url', 'content',)
 
 
-class AssessmentForm(forms.ModelForm):
+#class AssessmentForm(forms.ModelForm):
+#
+#    class Meta:
+#        model = Assessment
+#        fields = ('comment',)
 
-    class Meta:
-        model = Assessment
-        fields = ('comment',)
+
+class AssessmentForm(forms.Form):
+    submission = forms.IntegerField(required=True, widget=forms.HiddenInput())
+    comment = forms.CharField(
+                    label = _(u'Comment'),
+                    widget = forms.Textarea(attrs={'rows': '12', 'cols': '55'}),
+                    )
+    widgets = {
+               'ratings': forms.RadioSelect,
+               }
+
+    def __init__(self, submission, *args, **kwargs):
+        super(AssessmentForm, self).__init__(*args, **kwargs)
+        self.submission = submission
+        self.badge = submission.badge
+    
+        rubrics = submission.badge.rubrics.all()
+        ratings = []
+        for rubric in rubrics:
+            ratings.append(Rating(rubric=rubric))
+        self.ratings = ratings
 
 
 class RatingForm(forms.ModelForm):
