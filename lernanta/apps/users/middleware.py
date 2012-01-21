ea03178b7263f4364d5255b77942716c1ae57585
Zuzel Vera <zuzel.vp@gmail.com>
2012-01-21 15:12:50 -0500

    Bug fix.

--- lernanta/apps/users/middleware.py
+++ lernanta/apps/users/middleware.py
@@ -15,7 +15,8 @@ class ProfileExistMiddleware(object):
                 dashboard_url = reverse('dashboard')
                 profile_create_url = reverse('users_profile_create')
                 logout_url = reverse('users_logout')
-                valid_urls = (dashboard_url, profile_create_url, logout_url)
+                check_username_url = reverse('users_check_username')
+                valid_urls = (dashboard_url, profile_create_url, logout_url, check_username_url)
                 if request.path in valid_urls:
                     return None
                 for prefix in settings.NO_PROFILE_URLS:
