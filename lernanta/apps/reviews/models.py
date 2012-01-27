ee04ce96096a4d7dd37a3f3253f33d1d37d94fc9
Zuzel Vera <zuzel.vp@gmail.com>
2012-01-26 21:32:25 -0500

    Adding Reviewer and Review models for community review support.

---
+++ lernanta/apps/reviews/models.py
@@ -0,0 +1,26 @@
+import logging
+
+from django.db import models
+
+from drumbeat.models import ModelBase
+from richtext.models import RichTextField
+
+log = logging.getLogger(__name__)
+
+
+class Reviewer(ModelBase):
+    user = models.ForeignKey('users.UserProfile')
+
+    def __unicode__(self):
+        return unicode(self.user)
+
+
+class Review(ModelBase):
+
+    project = models.ForeignKey('projects.Project', related_name='reviews')
+    accepted = models.BooleanField(default=True)
+    content = RichTextField(config_name='rich', blank='False')
+
+    def __unicode__(self):
+        return 'Review for %s' % self.project.name
+
