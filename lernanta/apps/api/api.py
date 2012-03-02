5c25be2e2a1ee23f62332bd2f7e36c875cb35377
josmas <josmasflores@gmail.com>
2012-03-02 21:53:26 +0000

    add relations from porjects with badges and school, if any

--- lernanta/apps/api/api.py
+++ lernanta/apps/api/api.py
@@ -1,17 +1,9 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 from projects.models import Project
 from users.models import UserProfile
 from schools.models import School
 from badges.models import Badge
-
-
-class ProjectResource(ModelResource):
-    class Meta:
-        queryset = Project.objects.get_active()
-        fields = ['category', 'community_featured', 'created_on',
-                'duration_hours', 'end_date', 'featured', 'image', 'language',
-                'long_description', 'name', 'other', 'other_description',
-                'resource_uri', 'short_description', 'slug', 'start_date']
 
 
 class UserProfileResource(ModelResource):
@@ -28,3 +20,16 @@ class SchoolResource(ModelResource):
 class BadgeResource(ModelResource):
     class Meta:
         queryset = Badge.objects.all()
+
+
+class ProjectResource(ModelResource):
+    school = fields.ForeignKey(SchoolResource, 'school', null=True)
+    completion_badges = fields.ToManyField(BadgeResource, 'badges', null=True)
+
+    class Meta:
+        queryset = Project.objects.get_active()
+        fields = ['school', 'category', 'community_featured', 'created_on',
+                'duration_hours', 'end_date', 'featured', 'image', 'language',
+                'long_description', 'name', 'other', 'other_description',
+                'resource_uri', 'short_description', 'slug', 'start_date',
+                'completion_badges']
