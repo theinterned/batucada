9e8a5d533b854755de082a0dade56ab5390a4a19
Josmas Flores <josmasflores@gmail.com>
2012-04-06 17:02:00 +0100

    added api to supported nonlocales to avoid redirection to a particular language

--- lernanta/settings.py
+++ lernanta/settings.py
@@ -55,7 +55,8 @@ USE_I18N = True
 # calendars according to the current locale
 USE_L10N = True
 
-SUPPORTED_NONLOCALES = ('media', '.well-known', 'pubsub', 'broadcasts', 'ajax')
+SUPPORTED_NONLOCALES = ('media', '.well-known', 'pubsub', 'broadcasts', 'ajax',
+    'api',)
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
