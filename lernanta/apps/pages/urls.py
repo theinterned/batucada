3ecebb8585a68617186fe9aa67bf6f4c16f63d8e
Dirk Uys <dirkcuys@gmail.com>
2012-04-13 15:19:00 +0200

    removed view that only renders template and replaced it with a generic view

--- lernanta/apps/pages/urls.py
+++ lernanta/apps/pages/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url
+from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('pages.views',
-  url(r'^get-involved/$', 'get_involved_page',
+  url(r'^get-involved/$', direct_to_template,
+    {'template': 'pages/get_involved_page.html'},
     name='static_get_involved_page'),
   url(r'^jobs/$', 'jobs_page', name='static_jobs_page'),
   url(r'^(?P<slug>[\w\-\. ]+)/$', 'show_page', name='static_page_show'),
