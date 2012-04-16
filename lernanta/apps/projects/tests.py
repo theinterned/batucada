5e64b239d3355a16f047908e31e15798abd69392
Zuzel Vera <zuzel.vp@gmail.com>
2012-04-16 14:32:07 -0400

    Test challenge creation at groups/create/challenge/

--- lernanta/apps/projects/tests.py
+++ lernanta/apps/projects/tests.py
@@ -57,3 +57,22 @@ class ProjectTests(TestCase):
         self.assertRedirects(response, 
             '/%s/groups/%s/' % (self.locale, 'test-new-course'),
             target_status_code=200)
+
+    def test_challenge_creation(self):
+        """Test valid post request to challenge creation page"""
+        data = {
+            'name': 'Test New Challenge',
+            'short_description': 'This is my new challenge',
+            'long_description': '<p>The new challenge is about...</p>',
+            'language': 'en',
+        }
+        self.client.login(username=self.test_username,
+            password=self.test_password)
+        response = self.client.post('/%s/groups/create/challenge/' % (self.locale,),
+            data)
+        slug = 'test-new-challenge'
+        self.assertRedirects(response,
+            '/%s/groups/%s/' % (self.locale, slug),
+            target_status_code=200)
+        challenge = Project.objects.get(slug=slug)
+        self.assertEqual(challenge.category, Project.CHALLENGE)
