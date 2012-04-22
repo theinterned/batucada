5b3a161e618367a519832da7110df46e812ffc8f
Zuzel Vera <zuzel.vp@gmail.com>
2012-04-22 17:57:38 -0400

    Moving all tracking codes to the database.

--- lernanta/apps/drumbeat/context_processors.py
+++ lernanta/apps/drumbeat/context_processors.py
@@ -1,8 +1,14 @@
 from django.conf import settings
+from django.contrib.sites.models import Site
+
+from tracker.models import get_google_tracking_context
 
 
 def django_conf(request):
-    return {
+    context =  {
         'settings': settings,
         'request': request,
     }
+    site = Site.objects.get_current()
+    context.update(get_google_tracking_context(site))
+    return context
