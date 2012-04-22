a16702c88e0cf7ed62d0fd674869bf0c15d78e5e
Zuzel Vera <zuzel.vp@gmail.com>
2012-04-22 00:33:49 -0400

    Including index information for the list of challenges in a set.

--- lernanta/apps/schools/models.py
+++ lernanta/apps/schools/models.py
@@ -108,9 +108,7 @@ class ProjectSet(ModelBase):
     school = models.ForeignKey(School, blank=True, null=True,
         related_name="project_sets")
     projects = models.ManyToManyField('projects.Project', blank=True, null=True,
-        related_name="projectsets")
-    first_project = models.ForeignKey('projects.Project', blank=True, null=True,
-        related_name="starting_project_sets")
+        related_name="projectsets", through='schools.ProjectSetIndex')
     featured = models.BooleanField(default=False)
     image = models.ImageField(upload_to=projectsets_determine_image_upload_path,
         null=True, storage=storage.ImageStorage(), blank=True)
@@ -132,6 +130,16 @@ class ProjectSet(ModelBase):
         image_path = self.image.url if self.image else missing
         return image_path
 
+    def get_projects(self):
+        return self.projects.order_by('projectsetindex__index')
+
+    @property
+    def first_project(self):
+        try:
+            return self.projects.order_by('projectsetindex__index')[0]
+        except IndexError:
+            return None
+
     def _distinct_participants(self):
         return UserProfile.objects.filter(participations__project__projectsets=self,
                                     deleted=False,
@@ -150,3 +158,10 @@ class ProjectSet(ModelBase):
     def total_badges(self):
         return Badge.objects.filter(
             groups__projectsets=self).distinct().count()
+
+
+class ProjectSetIndex(ModelBase):
+    """Stores order of projects inside set"""
+    projectset = models.ForeignKey('schools.ProjectSet')
+    project = models.ForeignKey('projects.Project')
+    index = models.IntegerField()
