9ad44ffcc501bd8d4f5810e7b6deace95ff2a9a8
josmas <josmasflores@gmail.com>
2012-05-16 11:17:54 +0100

    adds an API_SERVER constant to settings to redirect any api calls to that address

--- lernanta/apps/api/middleware.py
+++ lernanta/apps/api/middleware.py
@@ -1,10 +1,22 @@
 from django.http import HttpResponsePermanentRedirect
+from django.conf import settings
 
 class APISubdomainMiddleware:
     def process_request(self, request):
         """Parse out the subdomain from the request and redirect to API"""
+        if settings.API_SERVER == None:
+          return
+
         host = request.META.get('HTTP_HOST', '')
         host_s = host.replace('www.', '').split('.')
-        if (host_s[0] == 'api'):
-          return HttpResponsePermanentRedirect('http://' + host_s[1] + '.' + host_s[2]
+
+        if request.is_secure():
+          protocol = 'https://'
+        else:
+          protocol = 'http://'
+
+        if ((host_s[0] == 'api') and request.path.startswith('/v')):
+          print 'Redirecting now'
+          return HttpResponsePermanentRedirect(protocol + settings.API_SERVER 
               + '/api' + request.path + '?' + request.GET.urlencode(safe='&'))
+        return
