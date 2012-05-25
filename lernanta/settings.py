765f57961b20f6c952ce752b543dbbaf39395a6b
Dirk Uys <dirkcuys@gmail.com>
2012-05-25 13:40:39 +0200

    moving tasks up, down and delete in multi task edit view

--- lernanta/settings.py
+++ lernanta/settings.py
@@ -112,7 +112,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'maintenancemode.middleware.MaintenanceModeMiddleware',
-    'commonware.middleware.HidePasswordOnException',
+    'commonware.middleware.ScrubRequestOnException',
     'commonware.middleware.FrameOptionsHeader',
     'django.middleware.locale.LocaleMiddleware',
     'users.middleware.ProfileExistMiddleware',
