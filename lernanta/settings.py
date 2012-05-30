ca0a9be27dde087e32d7b72f20b3bcebd8146cc7
Dirk Uys <dirkcuys@gmail.com>
2012-05-30 09:36:51 +0200

    multi task edit page - task edit and create!

--- lernanta/settings.py
+++ lernanta/settings.py
@@ -246,7 +246,7 @@ CKEDITOR_FILE_UPLOAD_EXTENSIONS = CKEDITOR_IMAGE_UPLOAD_EXTENSIONS + ['.pdf',
     '.avi','.rm','.mp3','.mp4','.wav','.aiff','.midi','.m4p']
 
 # Where the default image for sending to Gravatar
-DEFAULT_PROFILE_IMAGE = 'http://p2pu.org/static/images/member-missing.png'
+DEFAULT_PROFILE_IMAGE = 'https://p2pu.org/static/images/member-missing.png'
 
 # When set to True, if the request URL does not match any
 # of the patterns in the URLconf and it doesn't end in a slash,
