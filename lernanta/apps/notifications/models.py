fa9d6bdfa382aeaf012f0df08201b93381a86eee
Dirk Uys <dirkcuys@gmail.com>
2012-06-12 12:08:06 +0200

    added comment to send_notifications

--- lernanta/apps/notifications/models.py
+++ lernanta/apps/notifications/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 from tasks import SendNotifications
 
-def send_notifications(user_profiles, subject_template, body_template, context):
-    """ Asynchronously send email notifications to users """
-    SendNotifications.apply_async(
-        (user_profiles, subject_template, body_template, context))
+def send_notifications(user_profiles, subject_template, body_template,
+        template_context, reply_token=None):
+    """Asynchronously send email notifications to users
+    
+    user_profiles - the users to send the notification to
+    subject_template - the template to use for the subject
+    body_template - the template to use for the body
+    template_context - the context to use when rendering the template
+    reply_token - used to generate reply_to address reply+reply_token@from.org
+    """
+    args = (user_profiles, subject_template, body_template, template_context)
+    SendNotifications.apply_async(args)
