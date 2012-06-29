944d7a4084211c43dd5985d9dd0565d7ba498ee8
Dirk Uys <dirkcuys@gmail.com>
2012-06-29 09:42:17 +0200

    turned notifications and response into a bit of a module - still need to update replies app to use new functionality and also generate response tokens inside notifications module

--- lernanta/apps/notifications/models.py
+++ lernanta/apps/notifications/models.py
@@ -1,10 +1,20 @@
 from django.db import models
 
-from tasks import SendNotifications
+from tasks import SendNotifications, PostNotificationResponse
 
 import logging
 
 log = logging.getLogger(__name__)
+
+class ResponseToken(models.model):
+    """ Store response tokens and callbacks """
+
+    # Unique token used to route response coming in via email
+    response_token = models.CharField(max_length=32, blank=False)
+
+    # URL to call when receiving a response
+    response_callback = models.CharField(max_length=255, blank=False)
+
 
 def send_notifications(user_profiles, subject_template, body_template,
         template_context, reply_token=None):
@@ -24,3 +34,8 @@ def send_notifications(user_profiles, subject_template, body_template,
 
     log.debug("sending notification {0}".format(args))
     SendNotifications.apply_async(args)
+
+def post_notification_response(token, from_email, text):
+
+    args = (token, from_email, text,)
+    PostNotificationResponse.apply_async(args)
