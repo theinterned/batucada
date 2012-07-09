748a69caf2b69aeef9aea76955a10e3304df8761
Dirk Uys <dirkcuys@gmail.com>
2012-07-09 11:42:17 +0200

    updated metrics update task to run separate tasks for projects

--- lernanta/apps/tracker/tasks.py
+++ lernanta/apps/tracker/tasks.py
@@ -1,6 +1,8 @@
 from celery.task.schedules import crontab
 from celery.decorators import periodic_task
 
+from celery.task import Task
+
 from models import update_metrics_cache
 from projects.models import get_active_projects
 
@@ -11,4 +13,15 @@ def update_metrics():
     log = update_metrics.get_logger()
     log.debug('updating project pageview metrics')
     for project in get_active_projects():
+        update_metrics_cache(project)
+        SendNotifications.apply_async((project,))
+
+
+class UpdateCourseMetrics(Task):
+    """ Update metrics relevant to a specific project."""
+    name = 'notifications.tasks.UpdateCourseMetrics'
+    
+    def run(self, project, **kwargs):
+        log = self.get_logger(**kwargs)
+        log.debug('updating pageview metrics for {0}'.format(project.name))
         update_metrics_cache(project)
