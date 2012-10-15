9d0d06daf7b3c85f254a04ed335c3b83cf05962f
Dirk Uys <dirkcuys@gmail.com>
2012-10-15 14:22:03 +0200

    allow empty content updates

--- lernanta/apps/content2/forms.py
+++ lernanta/apps/content2/forms.py
@@ -3,7 +3,7 @@ from content2 import utils
 
 class ContentForm(forms.Form):
     title = forms.CharField()
-    content = forms.CharField(widget=forms.Textarea)
+    content = forms.CharField(widget=forms.Textarea, required=False)
 
     def clean_content(self):
         return utils.clean_user_content(self.cleaned_data['content'])
