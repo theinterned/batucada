b53dc993402c5b492fad18c91bed3dd196d75d18
Dirk Uys <dirkcuys@gmail.com>
2012-11-02 10:21:42 +0200

    dont scan whole session table to log user out

--- lernanta/apps/users/middleware.py
+++ lernanta/apps/users/middleware.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.conf import settings
+from django.contrib.auth import logout
 
 from users.models import UserProfile
 from l10n.urlresolvers import reverse
@@ -8,18 +9,22 @@ from l10n.urlresolvers import reverse
 class ProfileExistMiddleware(object):
 
     def process_request(self, request):
-        if request.user.is_authenticated():
-            try:
-                request.user.get_profile()
-            except UserProfile.DoesNotExist:
-                dashboard_url = reverse('dashboard')
-                profile_create_url = reverse('users_profile_create')
-                logout_url = reverse('users_logout')
-                check_username_url = reverse('users_check_username')
-                valid_urls = (dashboard_url, profile_create_url, logout_url, check_username_url)
-                if request.path in valid_urls:
+        if not request.user.is_authenticated():
+            return
+        try:
+            request.user.get_profile()
+            if profile.deleted = True:
+                logout(request)
+                return HttpResponseRedirect(reverse('splash'))
+        except UserProfile.DoesNotExist:
+            dashboard_url = reverse('dashboard')
+            profile_create_url = reverse('users_profile_create')
+            logout_url = reverse('users_logout')
+            check_username_url = reverse('users_check_username')
+            valid_urls = (dashboard_url, profile_create_url, logout_url, check_username_url)
+            if request.path in valid_urls:
+                return None
+            for prefix in settings.NO_PROFILE_URLS:
+                if request.path.startswith(prefix):
                     return None
-                for prefix in settings.NO_PROFILE_URLS:
-                    if request.path.startswith(prefix):
-                        return None
-                return HttpResponseRedirect(dashboard_url)
+            return HttpResponseRedirect(dashboard_url)
