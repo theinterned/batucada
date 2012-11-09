5d9f32e579a8de6e49260a3daa577035986d5900
Dirk Uys <dirkcuys@gmail.com>
2012-11-09 16:03:11 +0200

    management command for adding existing courses to course index

---
+++ lernanta/apps/projects/management/commands/export_project_listing.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+
+from projects.models import Project
+from signups.models import Signup
+from learn.models import add_course_listing, update_course_listing
+
+class Command(BaseCommand):
+    args = '<poll_id poll_id ...>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        listed = Project.objects.filter(
+            not_listed=False,
+            deleted=False,
+            archived=False,
+            under_development=False,
+            test=False
+        )
+
+        listed = listed.filter(
+            Q(category=Project.CHALLENGE)
+            | Q(sign_up__status=Signup.MODERATED)
+            | Q(sign_up__status=Signup.NON_MODERATED)
+        )
+
+        for project in listed:
+            project_tags = project.tags.all().values_list('name', flat=True)
+            args = dict(
+                course_url = "http://p2pu.org/en/groups/{0}/".format(project.slug),
+                title = project.name,
+                description = project.short_description,
+                data_url = "https://p2pu.org/en/groups/{0}/data".format(project.slug),
+                language = project.language,
+                thumbnail_url = "http://p2pu.org{0}".format(project.get_image_url()),
+                tags = project_tags
+            )
+            try:
+                add_course_listing(**args)
+            except:
+                update_course_listing(**args)
+
