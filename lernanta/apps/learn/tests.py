8ffd5285ed4f3f2df335c93199f3dc52d45cc2ea
Dirk Uys <dirkcuys@gmail.com>
2012-11-28 13:05:08 +0200

    update tests for learn module

--- lernanta/apps/learn/tests.py
+++ lernanta/apps/learn/tests.py
@@ -2,8 +2,17 @@ from django.test import Client
 from django.contrib.auth.models import User
 
 from users.models import create_profile
-from projects.models import Project
+from learn.models import add_course_listing
+from learn.models import update_course_listing
+from learn.models import remove_course_listing
 from learn.models import get_listed_courses
+from learn.models import create_list
+from learn.models import add_course_to_list
+from learn.models import remove_course_from_list
+from learn.models import get_courses_by_list
+from learn.models import get_courses_by_tags
+from learn.models import get_tags_for_courses
+from learn.models import get_active_languages
 
 from test_utils import TestCase
 
@@ -25,37 +34,122 @@ class ProjectTests(TestCase):
         self.user.set_password(self.test_password)
         self.user.save()
 
-    def test_get_listed_courses(self):
-        deleted_project = Project(deleted=True, test=False)
-        deleted_project.save()
-       
-        not_listed_project = Project(not_listed=True, test=False)
-        not_listed_project.save()
-       
-        under_dev_project = Project(name="under_dev:default", test=False)
-        under_dev_project.save()
+        self.test_course = {
+            "course_url": "http://p2pu.org/courses/1/", 
+            "title": "Course title", 
+            "description": "Short description", 
+            "data_url": "", 
+            "language": "en", 
+            "thumbnail_url": "http://p2pu.org/media/image.png", 
+            "tags": ["tag1", "tag2", "tag3"]
+        }
 
-        archived_project = Project(under_development=False, archived=True)
-        archived_project.save()
 
-        test_project = Project(under_development=False, test=True)
-        test_project.save()
+    def test_add_course_listing(self):
+        """ test that courses are added to the course list """
 
-        project = Project(name="listed", under_development=False, test=False)
-        project.save()
-       
-        listed_projects = get_listed_courses()
+        add_course_listing(**self.test_course)
+        listed_courses = get_listed_courses()
 
-        self.assertFalse(deleted_project in listed_projects)
-        self.assertFalse(not_listed_project in listed_projects)
-        self.assertFalse(under_dev_project in listed_projects)
-        self.assertFalse(test_project in listed_projects)
+        self.assertTrue(len(listed_courses) == 1)
+        self.assertTrue(self.test_course['course_url'] == listed_courses[0].url)
 
-    def test_register_course(self):
-        #TODO
-        pass
 
-    def test_update_course_info(self):
-        #TODO
-        pass
- 
+    def test_update_course(self):
+        """ test that course data gets updated """
+
+        add_course_listing(**self.test_course)
+        updated_course = {
+            "title": 'New Title',
+            "description": "New description",
+            "data_url": "http://example.com", 
+            "language": "es", 
+            "thumbnail_url": "http://p2pu.org/media/new_image.png"
+        }
+        update_course_listing(self.test_course['course_url'] ,**updated_course)
+        listed_courses = get_listed_courses()
+
+        self.assertTrue(len(listed_courses) == 1)
+        self.assertTrue(listed_courses[0].title == updated_course['title'])
+        self.assertTrue(listed_courses[0].description == updated_course['description'])
+        self.assertTrue(listed_courses[0].data_url == updated_course['data_url'])
+        self.assertTrue(listed_courses[0].language == updated_course['language'])
+        self.assertTrue(listed_courses[0].thumbnail_url == updated_course['thumbnail_url'])
+
+
+    def test_remove_course(self):
+        """ test that courses are removed from the course list """
+
+        add_course_listing(**self.test_course)
+        remove_course_listing(self.test_course['course_url'])
+        listed_courses = get_listed_courses()
+
+        self.assertTrue(len(listed_courses) == 0)
+
+
+    def test_course_list(self):
+        """ test that course can be added to and removed from a list """
+
+        add_course_listing(**self.test_course)
+        create_list("test_list", "Test List", "")
+        add_course_to_list(self.test_course["course_url"], "test_list")
+        course_list = get_courses_by_list("test_list")
+
+        self.assertTrue(len(course_list) == 1)
+
+        remove_course_from_list(self.test_course["course_url"], "test_list")
+        course_list = get_courses_by_list("test_list")
+
+        self.assertTrue(len(course_list) == 0)
+
+
+    def test_course_tags(self):
+        """ test that course tags works as expected """
+
+        add_course_listing(**self.test_course)
+        course_list = get_courses_by_tags(self.test_course["tags"])
+        tags = get_tags_for_courses(course_list)
+
+        self.assertTrue(len(course_list) == 1)
+        self.assertTrue(course_list[0].url == self.test_course["course_url"])
+        self.assertTrue(len(tags) == len(self.test_course["tags"]))
+
+
+    def test_course_language(self):
+        """ test that course languages are handled correctly """
+
+        add_course_listing(**self.test_course)
+
+        es_course = {
+            "course_url": "http://p2pu.org/courses/2/", 
+            "title": "Course title", 
+            "description": "Short description", 
+            "data_url": "", 
+            "language": "es", 
+            "thumbnail_url": "http://p2pu.org/media/image.png", 
+            "tags": ["tag1", "tag2", "tag3"]
+        }
+        add_course_listing(**es_course)
+
+        es_course['course_url'] = "http://p2pu.org/courses/3/"
+        add_course_listing(**es_course)
+
+        nl_course = {
+            "course_url": "http://p2pu.org/courses/4/", 
+            "title": "Course title", 
+            "description": "Short description", 
+            "data_url": "", 
+            "language": "nl", 
+            "thumbnail_url": "http://p2pu.org/media/image.png", 
+            "tags": ["tag1", "tag2", "tag3"]
+        }
+        add_course_listing(**nl_course)
+
+        course_list = get_listed_courses()
+        self.assertTrue(len(course_list) == 4)
+        
+        languages = get_active_languages()
+        self.assertTrue(len(languages) == 3)
+
+        #TODO get_courses_by_language()
+
