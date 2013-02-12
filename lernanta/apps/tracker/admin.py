01ee9c7fcdc7f806fea5475bcd6c7fb175a97d23
Dirk Uys <dirkcuys@gmail.com>
2013-02-12 13:05:30 +0200

    Remove pageviews from admin interface

--- lernanta/apps/tracker/admin.py
+++ lernanta/apps/tracker/admin.py
@@ -1,20 +1,6 @@
 from django.contrib import admin
 
-from tracker.models import PageView, GoogleAnalyticsTrackingCode, GoogleAnalyticsTracking
-
-
-class PageViewAdmin(admin.ModelAdmin):
-    date_hierarchy = 'access_time'
-    list_display = ('id', 'session_key', 'user', 'access_time',
-        'request_url', 'referrer_url', 'ip_address', 'time_on_page',
-        'user_agent')
-    list_filter = ('access_time',)
-    search_fields = ('id', 'session_key', 'user__username',
-        'request_url', 'referrer_url', 'ip_address', 'user_agent')
-
-
-admin.site.register(PageView, PageViewAdmin)
-
+from tracker.models import GoogleAnalyticsTrackingCode, GoogleAnalyticsTracking
 
 class GoogleAnalyticsTrackingCodeAdmin(admin.ModelAdmin):
     pass
