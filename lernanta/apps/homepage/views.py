fc8c922bfa0f03a275b78c3b6f558c2f695d614e
ercchy <erika@serox.si>
2013-07-24 12:44:20 +0200

    making changes to the new home page

--- lernanta/apps/homepage/views.py
+++ lernanta/apps/homepage/views.py
@@ -1,9 +1,24 @@
-# Create your views here.
+import random
+
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 
-def home(request):
-    context = {}
+from learn.models import get_courses_by_list
 
-    return render_to_response('homepage/home.html', context, context_instance=RequestContext(request))
+from processors import get_schools
+from processors import get_feed
+
+
+def home(request):
+    feed_entries = get_feed()
+    courses = get_courses_by_list("showcase")
+    featured_count = min(4,len(courses))
+    courses = random.sample(courses, featured_count)
+    schools = get_schools()
+
+    return render_to_response('homepage/home.html', {
+        'feed_entries':  feed_entries,
+        'courses': courses,
+        'schools': schools,
+    }, context_instance=RequestContext(request))
