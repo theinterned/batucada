6119f0810b90a3b6459700bb7e5bf0847abea6e1
Dirk Uys <dirkcuys@gmail.com>
2014-05-16 10:56:40 -0400

    Fix content escaping issue within pre blocks

--- lernanta/apps/content2/templatetags/content2_tags.py
+++ lernanta/apps/content2/templatetags/content2_tags.py
@@ -4,6 +4,7 @@ from django.template.defaultfilters import stringfilter
 import markdown
 import bleach
 from html5lib.tokenizer import HTMLTokenizer
+import re
 
 register = template.Library()
 
@@ -12,6 +13,9 @@ register = template.Library()
 def convert_content( markdown_text ):
     html = markdown.markdown(markdown_text, ['tables'])
     html = bleach.linkify(html, target="_blank", tokenizer=HTMLTokenizer)
+    regex = re.compile(r'(?P<preblock>\<pre\>.*?</pre\>)', re.IGNORECASE|re.DOTALL)
+    f = lambda m: re.sub(r'&lt;', '<', re.sub(r'&gt;', '>', re.sub(r'&amp;', '&', m.group(0))))
+    html = regex.sub(f, html)
     return html
 
 convert_content.is_safe = True
